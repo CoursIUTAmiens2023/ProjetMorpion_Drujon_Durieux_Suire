ecfbfd2924b78260546aa1ef07ffcee4f32860e3
Crimson-Ice <drujon.antonin@gmail.com>
2023-11-29 00:15:25 +0100

    oublie state mandatoryPawn

---
+++ src/StateManager/MandatoryPawn.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand"
+import { Pion } from "../App"
+
+interface useMandatoryPawnType {
+    mandatoryPawn: Pion[] | null
+    setMandatoryPawn: (pions: Pion[]) => void
+    resetMandatoryPawn: () => void
+}
+
+const useMandatoryPawn = create<useMandatoryPawnType>((set) => ({
+    mandatoryPawn: null,
+    setMandatoryPawn: (pions: Pion[]) => set({ mandatoryPawn: pions }),
+    resetMandatoryPawn: () => set({ mandatoryPawn: null }),
+}))
+
+export default useMandatoryPawn
